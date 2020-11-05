2f5863995a89f6f2e95b9642326409da9e41f04a
QVandenhove <quentin.vandenhove@viacesi.fr>
2020-11-05 14:08:46 +0100

    merge distant repo

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
-if (sizeof($_FILES) > 0){
-    foreach($_FILES as $key => $file)
-    $article_number = sizeof(scandir("./")) - 2;
-    mkdir("./".strval($article_number));
-    var_dump(move_uploaded_file($_FILES[$key]['tmp_name'], "./".$article_number."/".$_FILES[$key]["name"]));
-    $unzipper = new ZipArchive();
-    $unzipper->open("./".strval($article_number."/".$_FILES[$key]["name"]));
-    $unzipper->extractTo("./".strval($article_number."/"));
-    echo(json_encode(["response" => "Fichier reçus"]));
+
+var_dump($_FILES);
+    foreach ($_FILES as $file) {
+        $article_number = sizeof(scandir("./inputs")) - 1;
+        mkdir("./inputs/".strval($article_number));
+        $zip_path = "./inputs/".strval($article_number)."/site.rar";
+        move_uploaded_file($file['tmp_name'], $zip_path);
+        $unzipper = new ZipArchive();
+        chmod("./inputs/".strval($article_number), 0777);
+        chmod($zip_path, 0777);
+        var_dump($unzipper->open($zip_path) == true);
+        $unzipper->extractTo("./inputs/".strval($article_number)."/");
+        $unzipper->close();
+        echo(json_encode(["response" => "Fichier reçus"]));
+        // unlink("./inputs/".strval($article_number."/".$file["name"]));
+    }
 }else{
     echo(json_encode(["response" => "Fichier non traités"]));
 }
-
-var_dump($_FILES);
